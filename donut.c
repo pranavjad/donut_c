e347598f34adbf116d4fb5897c3ba4f3b84a027b
Pranav Jadhav <pra168109@gmail.com>
2023-11-04 00:14:58 -0400

    Changed 2d graphing to parametric

--- donut.c
+++ donut.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <math.h>
-int f(int x) {
-    return 5 * cos(x);
+int X(int t) {
+    return t;
 }
-// int[] xy_to_rc(int x, int y, int n) {
-//     int half_n = (n - 1) / 2;
-//     return {x + half}
-// }
+int Y(int t) {
+    return 5 * cos(t);
+}
+
 void graph() {
     int n = 21;
     int half_n = (n - 1) / 2;
@@ -18,20 +18,16 @@ void graph() {
             grid[r][c] = '.';
         }
     }
-    for (int x = -half_n; x <= half_n; x ++) {
-        int i = x + half_n;
-        printf("%d %d %d\n", x, i, half_n);
-        points[i][0] = x;
-        points[i][1] = f(x);
-        // printf("(%d, %d)\n", points[i][0], points[i][1]);
-        // printf("(%d, %d)\n", -points[i][1] + half_n, points[i][0] + half_n);
-        // printf("%d\n", points[i][1]);
-        int r = half_n - points[i][1];
-        int c = points[i][0] + half_n;
+    for (int t = -half_n; t <= half_n; t ++) {
+        int x = X(t);
+        int y = Y(t);
+        int r = half_n - y;
+        int c = x + half_n;
         if (r >= 0 && r < n && c >= 0 && c < n) {
-            grid[half_n - points[i][1]][points[i][0] + half_n] = '#';
+            grid[r][c] = '#';
         }
     }
+    
     for (int r = 0; r < n; r++) {
         for (int c = 0; c < n; c++) {
             printf(" %c ", grid[r][c]);
